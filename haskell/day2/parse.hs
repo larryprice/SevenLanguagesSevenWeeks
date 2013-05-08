fa4af96d23a62d148db199bfa66d06a31ad44330
Larry Price <larry.price.dev@gmail.com>
2013-05-08 10:11:48 -0400

    Modifying parse to work with less code.

--- haskell/day2/parse.hs
+++ haskell/day2/parse.hs
@@ -4,7 +4,4 @@ module Main where
 
   parse s =
     let (wholes, decimals) = break (== '.') s
-    in (fromIntegral (foldl addUpWholes 0 (strip wholes)) :: Float) + addUpDecimals (strip decimals)
-
-  addUpWholes carry x = 10*carry + digitToInt x
-  addUpDecimals a = read ("0." ++ a) :: Float
+    in read ((strip wholes) ++ "." ++ (strip decimals)) :: Float
